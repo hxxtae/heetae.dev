b8422d5c79b2525751e03bf3ceca71a7ab981018
hxxtae <fkdlxmfkdl@gmail.com>
2024-03-31 23:27:49 +0900

    :books: [Docs]: Apply library of classnames
    - fix component of Nav

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -2,13 +2,24 @@
 
 import Link from 'next/link';
 import S from './page.module.css';
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 export default function Nav() {
+  const pathname = usePathname();
+  console.log(pathname?.startsWith("/about"))
+
+  const navClass = (path: string) =>
+    classNames({
+      [S.nav_link]: true,
+      [S.active]: pathname?.startsWith(path) ? true : false
+    });
+
   return (
     <nav className={S.nav}>
-      <Link href={"/about"}>About</Link>
-      <Link href={"/projects"}>Projects</Link>
-      <Link href={"/resume"}>Resume</Link>
+      <Link className={navClass('/about')} href={"/about"}>About</Link>
+      <Link className={navClass('/projects')} href={"/projects"}>Projects</Link>
+      <Link className={navClass('/resume')} href={"/resume"}>Resume</Link>
     </nav>
   )
 }
