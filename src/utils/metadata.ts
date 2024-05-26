3fc17ba49abd6d83fe20cdeb1e76d6e1a7fdc15d
hxxtae <fkdlxmfkdl@gmail.com>
2024-05-26 23:42:55 +0900

    :book: feat: create metadata function

---
+++ src/utils/metadata.ts
@@ -0,0 +1,50 @@
+import { Metadata } from "next";
+
+interface MetadataProps {
+  title: string;
+  description: string;
+  path: string;
+  image?: string;
+  label1?: {
+    name: string;
+    data: string | number;
+  };
+  label2?: {
+    name: string;
+    data: string | number;
+  };
+}
+
+const webUrl = "https://heetae.dev";
+const defaultImage = `/main.png`;
+
+export default function metadata(props: MetadataProps): Metadata {
+  const { title, description: desc, path, image, label1, label2 } = props;
+  const description = desc + " | 희태의 개발 블로그, heetae.dev";
+  const images = webUrl + (image ?? defaultImage);
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: webUrl + path,
+      siteName: "miryang.dev",
+      images,
+      locale: "ko_KR",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images,
+    },
+    other: {
+      ["twitter:label1"]: label1?.name ?? "",
+      ["twitter:data1"]: label1?.data ?? "",
+      ["twitter:label2"]: label2?.name ?? "",
+      ["twitter:data2"]: label2?.data ?? "",
+    },
+  };
+}
