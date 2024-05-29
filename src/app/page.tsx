6bc2e27de89e812f580f12261cf8df3b579157ab
hxxtae <fkdlxmfkdl@gmail.com>
2024-05-29 20:29:05 +0900

    :books: docs: add favicon icon and manifest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import S from "./page.module.css";
+import { Metadata } from 'next';
+import metadata from '@/utils/metadata';
 
 export default function Home() {
   return (
@@ -7,4 +9,12 @@ export default function Home() {
       <Image className={S.home_logo} alt="h logo" src="/H_1.png" width={400} height={400} />
     </section>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  return metadata({
+    title: "Heetae | Frontend Engineer",
+    description: "김희태의 포트폴리오 웹 사이트",
+    path: `/`,
+  });
 }
